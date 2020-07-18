381803569041a4901ab059c909156d16dde2760e
Juan Gonzalez <jgu1@hotmail.com>
2020-07-17 21:54:55 -0400

    Change style of components

--- src/components/post/post.component.jsx
+++ src/components/post/post.component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import "./post.styles.scss";
 
+import CustomButton from "../custom-button/custom-button.component";
+
 const Post = ({ name, description, onDelete }) => (
     <tr>
         <td>{ name }</td>
         <td>{ description }</td>
-        <td><button onClick={onDelete}>Eliminar</button></td>
+        {/* <td><button onClick={onDelete}>Eliminar</button></td> */}
+        <td><CustomButton onClick={onDelete}>Eliminar</CustomButton></td>
     </tr>
 );
 
