a07e231b119171ed7dd8e535103533f33aa610cd
Juan Gonzalez <jgu1@hotmail.com>
2020-07-19 15:10:13 -0400

    Fix delete button

--- src/components/post/post.component.jsx
+++ src/components/post/post.component.jsx
@@ -5,17 +5,10 @@ import "./post.styles.scss";
 import Button from "react-bootstrap/Button";
 
 const Post = ({ name, description, onDelete }) => (
-	// <tr>
-	//     <td>{ name }</td>
-	//     <td>{ description }</td>
-	//     {/* <td><button onClick={onDelete}>Eliminar</button></td> */}
-	//     <td><CustomButton onClick={onDelete}>Eliminar</CustomButton></td>
-	// </tr>
 	<div className="post-row post">
 		<div className="post-col">{ name }</div>
 		<div className="post-col">{ description }</div>
-		{/* <div className="post-col"><button onClick={onDelete}>Eliminar</button></div> */}
-        <div className="post-col"><Button size="sm" variant="outline-danger">Eliminar</Button></div>
+        <div className="post-col"><Button onClick={onDelete} size="sm" variant="outline-danger">Eliminar</Button></div>
 	</div>
 );
 
